a6a3aadfcae576792d0f077aa2970157715039f1
ayushpatel1299 <patelayush.p1299@gmail.com>
2023-04-05 09:58:32 -0400

    new

---
+++ itis3135/hobby/scripts/availability.js
@@ -0,0 +1,9 @@
+function toggleContactCard() {
+			var contactCard = document.getElementById("contact-card");
+			if (contactCard.style.display === "none") {
+				contactCard.style.display = "block";
+			} else {
+				contactCard.style.display = "none";
+			}
+		}
+
